e76604b81e0ce23e1bd39da7df9e2f11c21f35e1
idontknow-316 <101419201+idontknow-316@users.noreply.github.com>
2022-06-05 08:23:43 +0545

    red color and dice added

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-
-      ),
-    ),
-  );
+ runApp(DiceApps());
 }
 
-class DicePage extends StatelessWidget {
+class DiceApps extends StatelessWidget {
+  const DiceApps({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('DICE'),
+      ),
+      body: SafeArea(
+
+      child: Row(
+
+        children:[
+          Expanded(
+            flex: 2,
+          child: Image.asset('assets/dice6.png')),
+          Expanded(
+            flex: 2,
+          child: Image.asset('assets/dice3.png')),
+          Expanded(
+              flex: 2,
+              child: Image.asset('assets/dice1.png')),
+          ]
+    ),
+      ),
+    );
   }
 }
